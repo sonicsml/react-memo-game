e44a5c20a728befc691e36112cf3a1ed6bf0b03f
Polovkov Dmitry <sonics.scs@gmail.com>
2021-11-10 13:37:20 +0300

    feat: add css, basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"}
+]
 function App() {
+  const [cards, setCards] = useState([])
+  const [terns, setTerns] = useState(0)
+  // suffle cards
+  const suffleCards = () => {
+    const suffledCards = [ ...cardImages, ...cardImages ]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+    
+    setCards(suffledCards)
+    setTerns(0)
+  }
+  console.log(cards, terns)
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Magic Match</h1>
-        <button>New Game</button>
-      </header>
+        <button onClick={suffleCards}>New Game</button>
+
+        <div className="card-grid">
+          {cards.map(card => (
+            <div className="card" key={card.id}>
+              <div>
+                <img className="fort" src={card.src} alt="card front" />
+                <img className="back" src="img/cover.png" alt="card back" />
+              </div>
+            </div>
+          ))}
+        </div>
+
     </div>
   );
 }
